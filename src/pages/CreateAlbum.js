af7659c42415d4b4e9d6b93ddfabf09008aa9621
Mehmet Ziya <mehmetziya82@gmail.com>
2022-01-13 11:06:44 +0100

    created images div on create album page

--- src/pages/CreateAlbum.js
+++ src/pages/CreateAlbum.js
@@ -1,15 +1,20 @@
+import { useState } from 'react'
 import Dropzone from '../components/Dropzone'
 import { useUserContext } from '../hooks/useUserContext'
 import { useImageContext } from '../context/ImageContext'
-import { useState } from 'react/cjs/react.development'
+import { useDeleteImage } from '../hooks/useDeleteImage'
 import { addDoc, collection } from 'firebase/firestore'
 import { db } from '../firebase/config'
+import Images from '../components/Images'
+import { useNavigate } from 'react-router-dom'
 
 const CreateAlbum = () => {
   const [error, setError] = useState(null)
   const [albumName, setAlbumName] = useState('')
   const { user } = useUserContext()
   const { images, setImages } = useImageContext()
+  const navigate = useNavigate()
+  const useDeleteObj = useDeleteImage()
   const handleSubmit = async (e) => {
     e.preventDefault()
     if (!images.length) {
@@ -23,15 +28,21 @@ const CreateAlbum = () => {
           photographerId: user.uid,
         })
         setImages([])
+        setAlbumName('')
+        navigate('/')
       } catch (e) {
         setError('Fail to upload album')
       }
     }
   }
+  const handleRemoveImage = (img) => {
+    useDeleteObj.remove(img.path, img.uuid)
+    setImages((images) => images.filter((image) => image.uuid !== img.uuid))
+  }
   return (
     <div className='form-container'>
       <div className='create-form'>
-        <h3>Create Album</h3>
+        <h3>Add Album</h3>
         <form onSubmit={handleSubmit}>
           <label>
             <span>Album name:</span>
@@ -42,11 +53,12 @@ const CreateAlbum = () => {
               value={albumName}
             />
           </label>
-          <Dropzone />
           <button type='submit' className='btn'>
             Upload
           </button>
         </form>
+        <Dropzone />
+        <Images images={images} onRemove={handleRemoveImage} />
         {error && <p>{error}</p>}
       </div>
     </div>
